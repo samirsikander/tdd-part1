55dae7877af2950e1b528dd092d5f607092f76ff
Samir Sikander <Samir Sikander@Samir-Laptop>
2015-01-03 19:02:55 +0500

    Chapter 9

--- src/Franc.java
+++ src/Franc.java
@@ -1,10 +1,16 @@
-public class Franc extends Money{
+public class Franc extends Money {
 
-	Franc(int amount) {
-		this.amount = amount;
+	private String currency;
+
+	Franc(int amount, String currency) {
+		super(amount, currency);
+		}
+
+	String currency() {
+		return currency;
 	}
 
 	Money times(int multiplier) {
-		return new Franc(amount * multiplier);
+		return Money.franc(amount * multiplier);
 		}
 }
