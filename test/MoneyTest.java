ded43684331b89261745baad360e284edead88e4
Samir Sikander <Samir Sikander@Samir-Laptop>
2015-01-03 20:00:19 +0500

    Chapter 11

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -19,8 +19,6 @@ public class MoneyTest {
 	@Test public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 		}
 	
@@ -28,9 +26,4 @@ public class MoneyTest {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 		}
-	
-	@Test public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-	}
-	
 }
