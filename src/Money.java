a7bdc96a2fa9beda84c00475daf06e0d91bb0f54
Samir Sikander <Samir Sikander@Samir-Laptop>
2015-01-03 20:51:52 +0500

    Chapter 14

--- src/Money.java
+++ src/Money.java
@@ -1,45 +1,44 @@
+public class Money implements Expression {
 
-public  class Money implements Expression {
-	
 	protected int amount;
 	protected String currency;
-	
+
 	Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
-		}
-		
+	}
+
 	Money times(int multiplier) {
 		return new Money(amount * multiplier, currency);
-		}
+	}
 
 	String currency() {
-	return currency;
+		return currency;
 	}
 
 	public String toString() {
 		return amount + " " + currency;
-		}
-	
+	}
+
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount
-		&& currency().equals(money.currency());
-		}
-	
+		return amount == money.amount && currency().equals(money.currency());
+	}
+
 	static Money dollar(int amount) {
 		return new Money(amount, "USD");
-		}
-	
-	static Money franc(int amount){
+	}
+
+	static Money franc(int amount) {
 		return new Money(amount, "CHF");
-		}
-	
+	}
+
 	Expression plus(Money addend) {
 		return new Sum(this, addend);
-		}
-	
-	public Money reduce(String to) {
-		return this;
-		}
+	}
+
+	public Money reduce(Bank bank, String to) {
+		int rate = bank.rate(currency, to);
+		return new Money(amount / rate, to);
+	}
 }
